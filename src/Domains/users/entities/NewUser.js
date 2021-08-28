fb08ab2bea9f2edd4b37a85562a7ab08d6101d9e
asdamwongmantap <asdamgrimson@gmail.com>
2021-08-28 11:59:12 +0700

    add module create user

---
+++ src/Domains/users/entities/NewUser.js
@@ -0,0 +1,26 @@
+class NewUser {
+    constructor({ username, password, fullname }) {
+      if (!username || !password || !fullname) {
+        throw new Error('NEW_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+      }
+   
+      if (typeof username !== 'string' || typeof password !== 'string' || typeof fullname !== 'string') {
+        throw new Error('NEW_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      }
+   
+      if (username.length > 50) {
+        throw new Error('NEW_USER.USERNAME_LIMIT_CHAR');
+      }
+   
+      if (!username.match(/^[\w]+$/)) {
+        throw new Error('NEW_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+      }
+   
+      this.username = username;
+      this.password = password;
+      this.fullname = fullname;
+    }
+  }
+   
+  module.exports = NewUser;
+  
